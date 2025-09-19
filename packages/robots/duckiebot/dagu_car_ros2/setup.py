d6af7b74448f38d51c1f15f387430a052a683879
eldgar <walikanani@gmail.com>
2025-09-18 23:13:40 -0600

    ros2: conversion carried out for car_cmd, kinematics, and velocity_to_pose

---
+++ packages/robots/duckiebot/dagu_car_ros2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+package_name = 'dagu_car_ros2'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Duckietown',
+    maintainer_email='maintainer@example.com',
+    description='ROS 2 port of Dagu car interface nodes.',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'kinematics_node = dagu_car_ros2.kinematics_node:main',
+            'velocity_to_pose_node = dagu_car_ros2.velocity_to_pose_node:main',
+            'car_cmd_switch_node = dagu_car_ros2.car_cmd_switch_node:main',
+        ],
+    },
+)
+
+

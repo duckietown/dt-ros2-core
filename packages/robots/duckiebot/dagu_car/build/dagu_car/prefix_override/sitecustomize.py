cbe7ec07e7a92976849a958a606596d463f31f33
eldgar <walikanani@gmail.com>
2025-09-23 17:26:51 -0600

    ros2: update dagu_car and car_interface; drop build/install/log

--- packages/robots/duckiebot/dagu_car/build/dagu_car/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/dan/duckietown-ros2-ws/dt-core/packages/robots/duckiebot/dagu_car/install'
